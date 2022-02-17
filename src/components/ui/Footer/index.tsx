451d5e982ec5599dda1df981f0ef40d3856fc8cf
Viacheslav Okhapkin <slava@welcomemedia.ru>
2022-02-17 15:27:08 +0300

    Add footer icons

--- src/components/ui/Footer/index.tsx
+++ src/components/ui/Footer/index.tsx
@@ -14,54 +14,56 @@ export const Footer: React.FC<IFooter> = () => {
         <footer className={cn(styles.footer, 'mt-auto')}>
             <Container>
                 <div className="row py-5">
-                    <div className="col-lg-3 pb-4 mb-2">
+                    <div className="col-lg-5 pb-4 mb-2">
                         <a className="site-logo d-inline-block mb-4" href="/">
                             <img src="/assets/img/logo.svg" alt="SAB Bank"/>
                         </a> 
-                        <a className={cn(styles.marketBtn, styles['apple-btn'], 'mr-3 mb-3')} href="#">
-                            <span className="mb-subtitle">Download on the</span>
-                            <span className="mb-title">App Store</span>
-                        </a>
-                        <a className="market-btn google-btn mr-3 mb-3" href="#">
-                            <span className="mb-subtitle">Download on the</span>
-                            <span className="mb-title">Google Play</span>
-                        </a>
-                        <p className="text-sm text-muted">&copy; Sabbank, 2021 Все права защищены.</p>
+                        <div>
+                            <a className="d-inline-block mb-4" href="/">
+                                <img src="/assets/img/App-Store-badge.svg" alt="SAB Bank"/>
+                            </a> 
+                            <a className="d-inline-block mb-4" href="/">
+                                <img src="/assets/img/Google-Play-badge.svg" alt="SAB Bank"/>
+                            </a> 
+                        </div>
+                        <p className="text-sm text-muted">&copy; Sabbank, 2022 Все права защищены.</p>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-7">
                         <div className="row">
-                        <div className="col-md-6">
-                            <h4 className="widget-title">Product</h4>
-                            <div className="widget widget-categories">
-                            <ul>
-                                <li><a href="#">How it works</a></li>
-                                <li><a href="#">Benefits</a></li>
-                                <li><a href="#">Features</a></li>
-                            </ul>
+                            <div className="col-md-4">
+                                <div className={styles.categoriesTitle}>Product</div>
+                                <ul className={styles.categoriesList}>
+                                    <li><a href="#">How it works</a></li>
+                                    <li><a href="#">Benefits</a></li>
+                                    <li><a href="#">Features</a></li>
+                                </ul>
                             </div>
-                        </div>
-                        <div className="col-md-6">
-                            <h4 className="widget-title">{t('Support')}</h4>                            
-                            <div className="widget widget-categories">
-                            <ul>
-                                <li><a href="#">Terms & Conditions</a></li>
-                                <li><a href="#">Privacy policy</a></li>
-                                <li><a href="#">FAQ</a></li>
-                            </ul>
+                            <div className="col-md-4">
+                                <div className={styles.categoriesTitle}>{t('Support')}</div>                            
+                                <ul className={styles.categoriesList}>
+                                    <li><a href="#">Terms & Conditions</a></li>
+                                    <li><a href="#">Privacy policy</a></li>
+                                    <li><a href="#">FAQ</a></li>
+                                </ul>
                             </div>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-3">
-                        <div className="widget">
-                        <h4 className="widget-title">Contact</h4>
-                            <ul>
-                                <li><a href="mailto:support@example.com">ihtop@zeoju.org</a></li>
-                                <li><a href="#">11-000-000</a></li>
-                            </ul>
-                            <a className="social-btn sb-instagram" href="#"><i className="socicon-instagram"></i></a>
-                            <a className="social-btn sb-facebook" href="#"><i className="socicon-facebook"></i></a>
-                            <a className="social-btn sb-youtube" href="#"><i className="socicon-youtube"></i></a>
+                            <div className="col-md-4">
+                                <div className={styles.categoriesTitle}>Contact</div>
+                                <ul className={styles.categoriesList}>
+                                    <li><a href="mailto:support@example.com">ihtop@zeoju.org</a></li>
+                                    <li><a href="#">11-000-000</a></li>
+                                </ul>
+                                <div>
+                                    <a className="d-inline-block mb-4" href="/">
+                                        <img src="/assets/img/instagram.svg" alt="SAB Bank"/>
+                                    </a> 
+                                    <a className="d-inline-block mb-4" href="/">
+                                        <img src="/assets/img/facebook.svg" alt="SAB Bank"/>
+                                    </a> 
+                                    <a className="d-inline-block mb-4" href="/">
+                                        <img src="/assets/img/youtube.svg" alt="SAB Bank"/>
+                                    </a> 
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>                
